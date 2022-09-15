d720a1b345d67e4c1838545244a29d0aae930702
WindHide <WindHide520@gmail.com>
2022-09-15 23:33:18 +0800

    [A] project init

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import 'element-plus/dist/index.css'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(store)
+app.use(router)
+app.use(ElementPlus, {
+    locale: zhCn, // 设置中文
+  })
+app.mount('#app')
