5c45e0b10a4bd2b44262eb0fabb36bf16ac6919b
WindHide <WindHide520@gmail.com>
2022-09-18 23:05:01 +0800

    [A] axios引入

---
+++ src/apis/Drugs_Request.ts
@@ -0,0 +1,9 @@
+import axios from "axios"
+import {ElNotification} from 'element-plus'
+
+axios.defaults.baseURL = '/api'
+
+// drugs - apis
+function QUERY_DRUGS_FOR_LIST(){
+    
+}
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 import ElementPlus from 'element-plus'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import 'element-plus/dist/index.css'
 
 const app = createApp(App)
+
+axios.defaults.baseURL = '/api'
+app.config.globalProperties.$axios = axios //全局注册，使用方法为:this.$axios   axios
+
 
 app.use(store)
 app.use(router)
