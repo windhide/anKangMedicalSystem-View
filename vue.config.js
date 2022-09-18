1d4ef5be23f597f02070fef3984519d792f17392
WindHide <WindHide520@gmail.com>
2022-09-18 15:26:04 +0800

    [A] 配置添加，端口改成8081

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true
 })
+module.exports = { 
+    devServer: {
+        open: false,
+        port: 8081, //端口
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080/', // 服务器server
+                ws: false, // proxy websockets 
+                changeOrigin: true, // needed for virtual hosted sites
+                pathRewrite: {
+                    '^/api': '' // rewrite path
+                }
+            },
+        },
+    }
+}
