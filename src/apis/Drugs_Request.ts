724c40bdc54cf2af1e8cdc610dba93babbc470e8
WindHide <WindHide520@gmail.com>
2022-09-19 22:28:20 +0800

    [A] 药品页面逻辑代码更改

--- src/apis/Drugs_Request.ts
+++ src/apis/Drugs_Request.ts
@@ -4,8 +4,8 @@ import {ElNotification} from 'element-plus'
 axios.defaults.baseURL = '/api'
 
 // drugs - apis
-export function QUERY_DRUGS_FOR_LIST(){
-    return axios.post("drugs/select/list").then((res:any) =>{
+export function QUERY_DRUGS_FOR_LIST(page:number){
+    return axios.post("drugs/select/list/"+page).then((res:any) =>{
         return res.data;
     })
 }
--- src/views/mainViews/rootViews.vue
+++ src/views/mainViews/rootViews.vue
@@ -5,7 +5,9 @@
         <el-menu :default-openeds="['1', '3']">
           <el-sub-menu index="1">
             <template #title>
-              <el-icon><message /></el-icon>Navigator One
+              <el-icon>
+                <message />
+              </el-icon>Navigator One
             </template>
             <el-menu-item-group>
               <template #title>Group 1</template>
@@ -22,7 +24,9 @@
           </el-sub-menu>
           <el-sub-menu index="2">
             <template #title>
-              <el-icon><icon-menu /></el-icon>Navigator Two
+              <el-icon>
+                <icon-menu />
+              </el-icon>Navigator Two
             </template>
             <el-menu-item-group>
               <template #title>Group 1</template>
@@ -39,7 +43,9 @@
           </el-sub-menu>
           <el-sub-menu index="3">
             <template #title>
-              <el-icon><setting /></el-icon>Navigator Three
+              <el-icon>
+                <setting />
+              </el-icon>Navigator Three
             </template>
             <el-menu-item-group>
               <template #title>Group 1</template>
@@ -62,9 +68,9 @@
       <el-header style="text-align: right; font-size: 12px">
         <div class="toolbar">
           <el-dropdown>
-            <el-icon style="margin-right: 8px; margin-top: 1px"
-              ><setting
-            /></el-icon>
+            <el-icon style="margin-right: 8px; margin-top: 1px">
+              <setting />
+            </el-icon>
             <template #dropdown>
               <el-dropdown-menu>
                 <el-dropdown-item>View</el-dropdown-item>
@@ -79,17 +85,24 @@
 
       <el-main>
         <el-scrollbar>
-          <el-table :data="drugsList">
-            <el-table-column prop="drugsId" label="药Id" width="100" />
-            <el-table-column prop="drugsName" label="药名" width="400" />
+          <el-table :data="drugsList" max-height="600">
+            <!-- <el-table-column prop="drugsId" label="药Id" width="100" /> -->
+            <el-table-column prop="drugsName" label="药名" width="350" />
             <el-table-column prop="drugsType.drugsTypeName" label="类型" width="100" />
-            <el-table-column prop="drugsSpecifications" label="规格" width="150"/>
-            <el-table-column prop="drugsUnit.drugsUnitName" label="单位"  width="100"/>
-            <el-table-column prop="drugsPlace" label="产地" width="300"/>
+            <el-table-column prop="drugsSpecifications" label="规格" width="150" />
+            <el-table-column prop="drugsUnit.drugsUnitName" label="单位" width="100" />
+            <el-table-column prop="drugsPlace" label="产地" width="300" />
             <el-table-column prop="drugsOriginPrice" label="原始价格" width="100" />
-            <el-table-column prop="drugsRetailPrice" label="零售价" width="100"/>
-            <el-table-column prop="drugsCreatTime" label="录入时间" />
+            <el-table-column prop="drugsRetailPrice" label="零售价" width="100" />
+            <el-table-column prop="drugsCreatTime" label="录入时间" width="320" />
+            <el-table-column fixed="right" label="操作">
+              <template #default="scope">
+                <el-button type="primary" :icon="Edit" circle />
+                <el-button type="danger" :icon="Delete" circle />
+              </template>
+            </el-table-column>
           </el-table>
+          <el-pagination background :page-size="pageSize" layout="prev, pager, next" :total="totals" @current-change="handleSizeChange" />
         </el-scrollbar>
       </el-main>
     </el-container>
@@ -98,21 +111,30 @@
 
 <script lang="ts" setup>
 import { reactive, ref } from 'vue'
-import { Menu as IconMenu, Message, Setting } from '@element-plus/icons-vue'
-import {QUERY_DRUGS_FOR_LIST} from "@/apis/Drugs_Request"
+import { Menu as IconMenu, Message, Setting, Delete, Edit } from '@element-plus/icons-vue'
+import { QUERY_DRUGS_FOR_LIST } from "@/apis/Drugs_Request"
 
 
-const item = {
-  date: '2016-05-02',
-  name: 'Tom',
-  address: 'No. 189, Grove St, Los Angeles'
+
+let drugsList: any = reactive([])
+let pageSize = ref(String);
+let totals = ref(String);
+
+QUERY_DRUGS_FOR_LIST(1).then(res => {
+  drugsList.push(...res.data);
+  pageSize.value = res.size;
+  totals.value = res.total;
+  console.log(res.size)
+  console.log(res.total)
+})
+
+const handleSizeChange = (val: number) => {
+  QUERY_DRUGS_FOR_LIST(val).then(res => {
+    drugsList.length = 0;
+    drugsList.push(...res.data);
+  })
 }
 
-const drugsList:any = reactive([])
-
-QUERY_DRUGS_FOR_LIST().then(res =>{
-  drugsList.push(...res);
-})
 </script>
 
 <style scoped>
@@ -121,16 +143,20 @@ QUERY_DRUGS_FOR_LIST().then(res =>{
   background-color: var(--el-color-primary-light-7);
   color: var(--el-text-color-primary);
 }
+
 .layout-container-demo .el-aside {
   color: var(--el-text-color-primary);
   background: var(--el-color-primary-light-8);
 }
+
 .layout-container-demo .el-menu {
   border-right: none;
 }
+
 .layout-container-demo .el-main {
   padding: 0;
 }
+
 .layout-container-demo .toolbar {
   display: inline-flex;
   align-items: center;
