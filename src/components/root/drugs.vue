345d60f4ed8bfab5b9fb2180f8a78ff263001712
WindHide <WindHide520@gmail.com>
2022-09-29 22:46:04 +0800

    [M] 样式修改

--- src/components/root/drugs.vue
+++ src/components/root/drugs.vue
@@ -234,36 +234,5 @@
         })
       }, 100);
     };
-    
     handleSizeChange(1); // 初始化数据
-    
     </script>
-    
-    <style scoped>
-    .layout-container-demo .el-header {
-      position: relative;
-      background-color: var(--el-color-primary-light-7);
-      color: var(--el-text-color-primary);
-    }
-    
-    .layout-container-demo .el-aside {
-      color: var(--el-text-color-primary);
-      background: var(--el-color-primary-light-8);
-    }
-    
-    .layout-container-demo .el-menu {
-      border-right: none;
-    }
-    
-    .layout-container-demo .el-main {
-      padding: 0;
-    }
-    
-    .layout-container-demo .toolbar {
-      display: inline-flex;
-      align-items: center;
-      justify-content: center;
-      height: 100%;
-      right: 20px;
-    }
-    </style>
